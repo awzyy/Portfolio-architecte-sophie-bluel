02e6d93a527d6438754fd24aecc857d25e23839d
awzyy <waysbdo@gmail.com>
2023-07-17 18:11:44 +0200

    api functions improvement

--- FrontEnd/js/api/api.js
+++ FrontEnd/js/api/api.js
@@ -1,20 +1,22 @@
-export function fetchWorks() {
-    return fetch('http://localhost:5678/api/works')
-      .then(response => response.json())
-      .catch(error => {
-        console.log('An error occurred', error);
-      });
+export async function fetchWorks() {
+    try {
+    const response = await fetch('http://localhost:5678/api/works');
+    return await response.json();
+  } catch (error) {
+    console.log('An error occurred', error);
+  }
   }
   
-  export function fetchCategories() {
-    return fetch('http://localhost:5678/api/categories')
-      .then(response => response.json())
-      .catch(error => {
-        console.log('An error occurred', error);
-      });
+  export async function fetchCategories() {
+    try {
+      const response = await fetch('http://localhost:5678/api/categories');
+      return await response.json();
+    } catch (error) {
+      console.log('An error occurred', error);
+    }
   }
   
-  export function deleteWork(id, accessToken) {
+  export async function deleteWork(id, accessToken) {
     return fetch(`http://localhost:5678/api/works/${id}`, {
       method: 'DELETE',
       headers: {
@@ -35,7 +37,7 @@ export function fetchWorks() {
       });
   }
   
-  export function createWork(image, title, category, accessToken) {
+  export async function createWork(image, title, category, accessToken) {
     const formData = new FormData();
     formData.append('image', image);
     formData.append('title', title);
